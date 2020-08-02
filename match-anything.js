96a10a137dc6f1277bbadc101aa5fb919a49a10e
Ghayyas Mubashir <ghayyasmubashir@gmail.com>
2020-08-02 22:39:48 +0500

    ** Match anything with Wildcard Period **

---
+++ match-anything.js
@@ -0,0 +1,10 @@
+// Match anything with Wildcard Period
+
+
+let example = "Let's have fun with regular expression";
+
+let unRegex = /.un/;
+
+let result =  unRegex.test(example);
+
+console.log(result);  //true
